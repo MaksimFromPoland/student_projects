aa446e4ca048f7e22e847aae0351c986f3d74a43
PISICZKIN <grut24121932@gmail.com>
2026-03-12 15:25:33 +0100

    FIRST COMMIT

---
+++ SM_Z_25/proekt_na_zaliczenie_1.cpp
@@ -0,0 +1,362 @@
+﻿#include <iostream>
+#include <vector>  
+#include <string>
+#include<chrono>
+#include<thread>
+#include <fstream>
+
+using namespace std;
+using namespace chrono;
+
+class produkt                                                     //Klassa bazowa "Produkt"
+{
+public:
+	int id;
+	double cena, mass;
+	string nazw;
+
+	void getProd()                                                //funkcja wyprowadzająca produkt na konsolę
+	{
+		cout << "Produkt: " << nazw << ", cena: " << cena << ", id: " << id << endl;
+	}
+};
+
+class prodMlecz : public produkt                                  //Produkty mlaczne
+{
+public:
+	void setprodMlecz(int i, double c, string naz)
+	{
+		id = i;
+		cena = c;
+		nazw = naz;
+	}
+};
+
+class prodOW : public produkt                                     //Produkty dla ktorych potrzebno dodac wage(warzywa i owoce)
+{
+public:
+	void setprodOW(int i, double c, string naz, double m)
+	{
+		id = i;
+		cena = c;
+		nazw = naz;
+		mass = m;
+	}
+};
+
+class prodMiesne : public produkt                                //Produkty miesne
+{
+public:
+	void setprodMiesne(int i, double c, string naz)
+	{
+		id = i;
+		cena = c;
+		nazw = naz;
+	}
+};
+
+double getSum(const vector<produkt>& koszyk, const vector<prodOW>& koszykOW)       //Podsumowanie rachunku
+{
+	double sum = 0, sumOW = 0;
+	for (auto& p : koszyk)
+	{
+		sum += p.cena;
+	}
+	for (auto& p : koszykOW)
+	{
+		sumOW = sumOW + (p.cena * p.mass);
+	}
+	return sum + sumOW;
+}
+
+int main()
+{
+	int wybor, wyborP, wyborPR, blik;
+	double got, sum = 0, reszta = 0, kilo = 0;
+	vector <produkt> lista;      //Lista produktow
+	vector <produkt> koszyk;     //Koszyk
+	vector <prodOW> koszykOW;    //Koszyk dla produktow potrzebujacych
+
+	prodMlecz p1;                                      //wypełnianie klasy produktami
+	p1.setprodMlecz(1, 6.10, "Mleko 3,2%");
+	prodMlecz p2;
+	p2.setprodMlecz(2, 4.50, "Smietana 18%");
+	prodMlecz p3;
+	p3.setprodMlecz(3, 4.36, "Twarog (poltlysty)");
+
+	prodOW pOW4;
+	pOW4.setprodOW(4, 6.12, "Banany", kilo);
+	prodOW pOW5;
+	pOW5.setprodOW(5, 4.25, "Jablka", kilo);
+	prodOW pOW6;
+	pOW6.setprodOW(6, 9.25, "Granat", kilo);
+	prodOW pOW7;
+	pOW7.setprodOW(7, 2.05, "Ziemnieki", kilo);
+	prodOW pOW8;
+	pOW8.setprodOW(8, 4.50, "Papryka", kilo);
+	prodOW pOW9;
+	pOW9.setprodOW(9, 3.40, "Cebula", kilo);
+
+	prodMiesne p10;
+	p10.setprodMiesne(10, 5.69, "Parufki");
+	prodMiesne p11;
+	p11.setprodMiesne(11, 9.76, "Szunka");
+	prodMiesne p12;
+	p12.setprodMiesne(12, 11.96, "Kilbasa");
+
+	lista.push_back(p1);      //wypełnianie vektora produktami
+	lista.push_back(p2);
+	lista.push_back(p3);
+	lista.push_back(pOW4);
+	lista.push_back(pOW5);
+	lista.push_back(pOW6);
+	lista.push_back(pOW7);
+	lista.push_back(pOW8);
+	lista.push_back(pOW9);
+	lista.push_back(p10);
+	lista.push_back(p11);
+	lista.push_back(p12);
+
+	cout << "=====WITAMY W SKLEPU PRODUKTOWYM=====\n";
+	cout << "      ===NASZ LIST PRODUKTOW===\n";
+	cout << "Produkty mleczne:\n";
+	p1.getProd();                                      //Wywod listy produktow wykorzystajac funkcje
+	p2.getProd();
+	p3.getProd();
+	cout << endl;
+	cout << "Produkty owocowe i warzywowe (cena za kilogramm):\n";
+	pOW4.getProd();
+	pOW5.getProd();
+	pOW6.getProd();
+	pOW7.getProd();
+	pOW8.getProd();
+	pOW9.getProd();
+	cout << endl;
+	cout << "Produkty miesne:\n";
+	p10.getProd();
+	p11.getProd();
+	p12.getProd();
+	cout << endl;
+
+	do
+	{
+		bool znal = false;
+		cout << "Wybierz co chcesz zrobic: \n";
+		cout << "1. Dodac produkt do koszyka.\n";
+		cout << "2. Usunac produkt z koszyka.\n";
+		cout << "3. Obejrzyc koszyk.\n";
+		cout << "4. Zobaczyc list produktow.\n";
+		cout << "5. Przejdz do kassy, aby zaplacic.\n";
+		cout << "Twoja opcja: \n";
+		cin >> wybor;
+
+		switch (wybor)
+		{
+		case 1:                                 //Dodanie produktu do koszyka
+			cout << "Wprowadz jaki produkt chcesz dodac (id):";
+			cin >> wyborPR;
+			for (auto& p : lista)
+			{
+				if (p.id == wyborPR)
+				{
+					if (p.id <= 9 && p.id >= 4)
+					{
+						cout << "Ile kilogramow checesz wziac: ";
+						cin >> kilo;
+						prodOW prod = (prodOW&)p;
+						prod.mass = kilo;
+						koszykOW.push_back(prod);
+						znal = true;
+						cout << "Dodalesz produkt do koszyka.\n";
+					}
+					else
+					{
+						koszyk.push_back(p);
+						znal = true;
+						cout << "Dodalesz produkt do koszyka.\n";
+					}
+					cout << endl;
+					break;
+				}
+			}
+			if (!znal)
+			{
+				cout << "Nie znalieziono takiego produktu.\n";
+			}
+			cout << endl;
+			break;
+
+		case 2:                    //Usuninie produktu z koszyka
+			cout << "Wprowadz jaki produkt chcesz usunac (id):";
+			cin >> wyborPR;
+			for (auto p = koszyk.begin(); p != koszyk.end(); ++p)
+			{
+				if (p->id == wyborPR)
+				{
+					koszyk.erase(p);
+					znal = true;
+					break;
+				}
+			}
+			for (auto p = koszykOW.begin(); p != koszykOW.end(); ++p)
+			{
+				if (p->id == wyborPR)
+				{
+					koszykOW.erase(p);
+					znal = true;
+					cout << endl;
+					break;
+				}
+			}
+			if (!znal)
+			{
+				cout << "Nie znalieziono takiego produktu.\n";
+			}
+			cout << endl;
+			break;
+
+		case 3:                      //Ogladnie koszyka
+			cout << "Twoj koszyk jest: \n";
+			for (auto& p : koszyk)
+			{
+				p.getProd();
+			}
+			for (auto& p : koszykOW)
+			{
+				p.getProd();
+			}
+			cout << endl;
+			sum = getSum(koszyk, koszykOW);
+			cout << "Summa rachunku jest: " << sum << endl;
+			cout << endl;
+			break;
+
+		case 4:                     //Ogladanie listy produktow
+			cout << "Produkty mleczne:\n";
+			p1.getProd();
+			p2.getProd();
+			p3.getProd();
+			cout << endl;
+			cout << "Produkty owocowe i warzywowe (cena za kilogramm):\n";
+			pOW4.getProd();
+			pOW5.getProd();
+			pOW6.getProd();
+			pOW7.getProd();
+			pOW8.getProd();
+			pOW9.getProd();
+			cout << endl;
+			cout << "Produkty miesne:\n";
+			p10.getProd();
+			p11.getProd();
+			p12.getProd();
+			cout << endl;
+			break;
+
+		case 5:                     //Przejszcie do kasy
+			break;
+
+		default:
+			cout << "Niepoprawny wybor!\n";
+		}
+	} while (wybor != 5);
+
+	cout << "Poczekaj na swoja kolej...\n";
+	this_thread::sleep_for(seconds(7));      //Symulacja czekania w kolejce
+	cout << endl;
+
+	sum = getSum(koszyk, koszykOW);      //Podsumowanie rachunku
+
+	do
+	{
+		cout << "Suma rachunku jest: " << sum << endl;
+		cout << endl;
+		cout << "W jaki sposob bedziesz oplacac: \n";
+		cout << "1. Karta.\n";
+		cout << "2. Gotowka.\n";
+		cin >> wybor;
+
+		switch (wybor)
+		{
+		case 1:                          //Opcja oplaty karta
+			do
+			{
+				cout << "Czym bedzisz oplacac: \n";
+				cout << "1. Karta fizyczna.\n";
+				cout << "2. Blikiem\n";
+				cin >> wyborP;
+
+				if (wyborP == 1)         //Opcja oplaty karta fizyczna
+				{
+					cout << "Poczekaj na tranzakcje...\n";
+					this_thread::sleep_for(seconds(3));    //Symulacja tranzakcji
+					cout << "Koszyk zostal oplacony!\n";
+				}
+				else if (wyborP == 2)   //Opcja oplaty blikiem
+				{
+					do
+					{
+						cout << "Wprowadz blik: ";
+						cin >> blik;
+						if (blik < 100000 || blik > 999999)
+						{
+							cout << "Blik jest nie poprawny! Sprobuj ponownie.\n";
+						}
+						else cout << "Koszyk zostal oplacony!\n";
+					} while (blik < 100000 || blik > 999999);
+				}
+				else cout << "Niepoprawna opcja!\n";
+			} while (wyborP != 1 && wyborP != 2);
+			break;
+
+		case 2:              //Opcja oplaty gotowka
+			do
+			{
+				cout << "Wprowadz sume gotowki:";
+				cin >> got;
+				if (got == sum)
+				{
+					cout << "Dzienki za oplate bez reszty :)\n";
+				}
+				else if (got < sum)
+				{
+					cout << "Nie wstarczy gotowki!\n";
+				}
+				else
+				{
+					reszta = got - sum;
+					cout << "Reszta jest: " << reszta << endl;
+				}
+			} while (got < sum);
+			break;
+
+		default:
+			cout << "Niepoprawna opcja!\n";
+		}
+	} while (wybor != 1 && wybor != 2);
+
+	ofstream rachunek("rachunek.txt", ios::app);
+	rachunek << "===Rachunek===\n";
+	rachunek << "Suma rachunku jest: ";
+	rachunek << sum;
+	rachunek << "\n";
+	if (wybor == 1)
+	{
+		rachunek << "Rachunek zostal oplacony karta albo blikiem.\n";
+		rachunek << "\n";
+	}
+	else
+	{
+		rachunek << "Suma podanej gotuwki: ";
+		rachunek << got;
+		rachunek << "\n";
+		rachunek << "Reszta: ";
+		rachunek << reszta;
+		rachunek << "\n";
+		rachunek << "\n";
+	}
+
+	cout << "Dzienki za zakupy! Przychodzcie do nas ponowie.\n";
+	return 0;
+}
+
+
---
+++ SM_Z_25/spidoznyje_koziawki.cpp
@@ -0,0 +1,193 @@
+﻿#include <iostream>
+#include <vector>  
+#include <string>
+#include <chrono>
+#include <thread>
+#include <fstream>
+using namespace std;
+using namespace chrono;
+
+class hero
+{
+public:
+    string imie;
+    int hp;
+    int ataka;
+};
+
+class warior : public hero 
+{
+public:
+    void setWarior(string i, int h, int a)
+    {
+        imie = i;
+        hp = h;
+        ataka = a;
+    }
+};
+
+class mage : public hero
+{
+private:
+    int fireBallAtak;
+public:
+    void setMage(string i, int h, int a, int f)
+    {
+        imie = i;
+        hp = h;
+        ataka = a;
+        fireBallAtak = f;
+    }
+};
+
+class paladin : public hero
+{
+private:
+    int shilde;
+public:
+    void setPaladin(string i, int h, int a, int s)
+    {
+        imie = i;
+        hp = h;
+        ataka = a;
+        shilde = s;
+    }
+};
+
+int main() 
+{
+    int wybor, wyborFight1, wyborFight2, wyborGra;
+    vector <warior> gracz1W;
+    vector <warior> gracz2W;
+    vector <mage> gracz1M;
+    vector <mage> gracz2M;
+    vector <paladin> gracz1P;
+    vector <paladin> gracz2P;
+
+    warior w;
+    w.setWarior("Warior", 5000, 800);
+    mage m;
+    m.setMage("Mage", 3000, 1100, 500);
+    paladin p;
+    p.setPaladin("Paladin", 6000, 500, 600);
+
+    cout << "===== WITAMY W GRIE 'HEROSE' =====\n";
+    cout << "Wybierz opcje:\n";
+    cout << "1. Rozpoczac gru.\n";
+    cout << "2. Wyjszc z programmu.\n";
+    cin >> wybor;
+
+    switch (wybor)
+    {
+    case 1:
+        do
+        {
+            cout << "Wybierz bochatera za ktorego bedzie grac pierwszy zawodnik:\n";
+            cout << "1. Warrior\n";
+            cout << "2. Mage\n";
+            cout << "3. Paladin\n";
+            cin >> wyborFight1;
+
+            switch (wyborFight1)
+            {
+            case 1:
+                gracz1W.push_back(w);
+                break;
+
+            case 2:
+                gracz1M.push_back(m);
+                break;
+
+            case 3:
+                gracz1P.push_back(p);
+                break;
+
+            default:
+                cout << "Niepoprawna opcja!\n";
+                break;
+            }
+        } while (wyborFight1 != 1 && wyborFight1 != 2 && wyborFight1 != 3);
+
+        do
+        {
+            cout << "Wybierz bochatera za ktorego bedzie grac drugi zawodnik:\n";
+            cout << "1. Warrior\n";
+            cout << "2. Mage\n";
+            cout << "3. Paladin\n";
+            cin >> wyborFight2;
+
+            switch (wyborFight2)
+            {
+            case 1:
+                gracz2W.push_back(w);
+                break;
+
+            case 2:
+                gracz2M.push_back(m);
+                break;
+
+            case 3:
+                gracz2P.push_back(p);
+                break;
+
+            default:
+                cout << "Niepoprawna opcja!\n";
+                break;
+            }
+        } while (wyborFight2 != 1 || wyborFight2 != 2 || wyborFight2 != 3);
+
+        cout << "Chodzi pierwszy zawodnik.\n";
+        cout << "Co chcesz zrobic:\n";
+        cout << "1. Atakowac poprostu\n";
+        cout << "2. Atakowac z uzywaniem specjalnej ataki\n";
+        cin >> wyborGra;
+        cout << endl;
+
+        if (wyborGra == 1)
+        {
+            if (!gracz1W.empty())
+            {
+                if (!gracz2W.empty())
+                {
+                    gracz2W[0].hp -= gracz1W[0].ataka;
+                    cout << "HP przeciwnika: " << gracz2W[0].hp << endl;
+                    cout << endl;
+                }
+                else if (!gracz2M.empty())
+                {
+                    gracz2M[0].hp -= gracz1W[0].ataka;
+                    cout << "HP przeciwnika: " << gracz2M[0].hp << endl;
+                    cout << endl;
+                }
+                else if (!gracz2P.empty())
+                {
+
+                }
+            }
+            else if (!gracz1M.empty())
+            {
+
+            }
+            else if (!gracz2P.empty())
+            {
+
+            }
+
+        }
+
+
+
+
+
+        break;
+
+    case 2:
+        break;
+
+    default:
+        cout << "Niepoprawna opcja\n";
+        break;
+    }
+
+    return 0;
+}
